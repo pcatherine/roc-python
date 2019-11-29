e8c62b0fdfee5aa997fd14ef2cc6a6e7492420c8
Caroline Pereira <itscarolinep@hotmail.com>
2019-11-28 21:02:45 -0300

    Adicionando arquivo de treinamento e classificacao. Update requirements.txt

---
+++ classifier.py
@@ -0,0 +1,113 @@
+import pickle
+from nltk import word_tokenize
+from nltk.classify import ClassifierI
+from statistics import mode
+import json
+
+# Ensemble model
+class EnsembleClassifier(ClassifierI):
+    
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+    
+    # Retorna qual classificador será usado, baseado na melhor acurácia
+    def classify(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        return mode(votes)
+
+    # Retorna a confiança na classificação 
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes / len(votes)
+        return conf 
+
+# Carrega os modelos pré-treinados
+def load_model(file_path): 
+    classifier_file = open(file_path, "rb")
+    classifier = pickle.load(classifier_file)
+    classifier_file.close()
+    return classifier
+
+# Load de palavras e features do arquivo de treinamento
+pck = open('word_features.pickle','rb')
+word_features = pickle.load(pck)
+
+def find_features(document):
+    words = word_tokenize(document)
+    features = {}
+    for word in word_features:
+        features[word] = (word in words)
+    return features
+
+# Função para análise de sentimento e confiança
+def sentiment_analysis(comments):
+    feats = []
+    classification = []
+    confidence = []
+    
+    # Extrai as features para cada review
+    for line in comments:
+        feats.append(find_features(line['review']))
+
+    # Volta a classificação e confiança para cada review
+    for feat in feats:
+        classification.append(ensemble.classify(feat))
+        confidence.append(ensemble.confidence(feat))
+    
+    lenght_class = len(classification)
+    lenght_conf = len(confidence)
+    class_dict = {}
+    cn, cp, cont_p, cont_n = 0, 0, 0, 0
+    for cl in classification:
+        if cl == 'pos': 
+            cont_p += 1
+        if cl == 'neg': 
+            cont_n += 1
+    res1 = cont_p/lenght_class
+    res2 = cont_n/lenght_class
+
+    return classification, res1, res2
+    # return log_reg.classify(feats
+    # return onb.classify(feats)
+    # return rf.classify(feats)
+    # return sgd.classify(feats)
+    # return svc.classify(feats)
+
+# Logistic Regression 
+log_reg = load_model('saved_files/log_reg.pickle')
+
+# Original Naive Bayes
+onb = load_model('saved_files/onb.pickle')
+
+# Random Forest 
+rf = load_model('saved_files/rf.pickle')
+
+# Stochastic Gradient Descent
+sgd = load_model('saved_files/sgd.pickle')
+
+# Support Vector Classifier 
+svc = load_model('saved_files/svc.pickle')
+
+# Ensemble classifier 
+ensemble = EnsembleClassifier(log_reg, onb, rf, sgd, svc)
+
+if __name__ == "__main__" : 
+    comments = [
+        {"review":"This new show popped up on Netflix so we decided to watch an episode, 5 hours later the whole season came to an end. Really enjoyable dark comedy, drama, crime all wrapped up in to one. Great performances from both the ladies. If you are looking for the 'who done it?' type of show you found it, however there is a slight twist. How does one reveal what they know. Enjoy it, looking forward to season 2."},
+        {"review":"I know too much about grief and this story explains so many different stages of grief that are not talked about. The story has so many twist and turns and it's well worth the watch"},
+        {"review":"The worst show i have ever seen"},
+        {"review":"You can really see great acting come out with a good script, dialogue, and story line. Christina Applegate is so good this! You literally never know what's going to happen next in this show. It's a binger!"},
+        {"review":"It was bad"}
+    ]
+    result = sentiment_analysis(comments)
+    print("Resultado: ", result[0])
+    print("Res1: ", result[1])
+    print("Res2: ", result[2])
---
+++ train.py
@@ -0,0 +1,151 @@
+import nltk
+import random
+import re
+import os
+import pickle
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression,SGDClassifier
+from sklearn.svm import SVC
+from sklearn.ensemble import VotingClassifier
+
+# Leitura de dataset rotulado
+# Modifique o path para o seu diretório
+files_pos = os.listdir('dataset/train/pos')
+files_pos = [open('dataset/train/pos/'+f, 'r', encoding="utf8").read() for f in files_pos]
+files_neg = os.listdir('dataset/train/neg')
+files_neg = [open('dataset/train/neg/'+f, 'r', encoding="utf8").read() for f in files_neg]
+
+# Baixando corpora necessários
+# Descomente para rodar a primeira vez
+# nltk.download('averaged_perceptron_tagger')
+# nltk.download('stopwords')
+# nltk.download('punkt')
+
+all_words = []
+documents = []
+
+# Criando lista de stopwords
+stop_words = list(set(stopwords.words('english')))
+
+# J - adjetivo
+# R - advérbio
+# V - verbo
+bag_of_words = ["J"]
+
+for p in files_pos:
+    # Arquivos com rótulo positivo
+    # Criando uma tupla com dois elementos, o primeiro é a review e o segundo o rótulo
+    documents.append((p, "pos"))
+    
+    # Removendo pontuação
+    cleaned = re.sub(r'[^(a-zA-Z)\s]','', p)
+    
+    # Tokenização 
+    tokenized = word_tokenize(cleaned)
+    
+    # Removendo stopwords 
+    stopped = [word for word in tokenized if not word in stop_words]
+    
+    # PoS-tagging
+    pos = nltk.pos_tag(stopped)
+    
+    # Criando o bag of words dos adjetivos permitidos
+    for word in pos:
+        if word[1][0] in bag_of_words:
+            all_words.append(word[0].lower())
+   
+for n in files_neg:
+    # Arquivos com rótulo negativo
+    # Criando uma tupla com dois elementos, o primeiro é a review e o segundo o rótulo
+    documents.append((n, "neg"))
+
+    # Removendo pontuação
+    cleaned = re.sub(r'[^(a-zA-Z)\s]','', n)
+
+    # Tokenização
+    tokenized = word_tokenize(cleaned)
+
+    # Removendo stopwords
+    stopped = [word for word in tokenized if not word in stop_words]
+
+    # PoS-tagging
+    neg = nltk.pos_tag(stopped)
+
+    # Criando o bag of words dos adjetivos permitidos
+    for word in neg:
+        if word[1][0] in bag_of_words:
+            all_words.append(word[0].lower())
+
+print("PASSOU")
+# TF-IDF
+all_words = nltk.FreqDist(all_words)
+
+# Lista das 5000 palavras mais frequentes
+word_features = list(all_words.keys())[:5000]
+pck = open("word_features.pickle", "wb")
+pickle.dump(word_features,pck)
+pck.close()
+
+print("PASSOU2")
+
+# Criando um dicionário de features
+def find_features(document):
+    words = word_tokenize(document)
+    features = {}
+    for word in word_features:
+        features[word] = (word in words)
+    return features
+
+# Features para cada review
+feature_set = [(find_features(rev), category) for (rev, category) in documents]
+ 
+random.shuffle(feature_set)
+
+training_set = feature_set[:20000]
+testing_set = feature_set[20000:]
+
+
+# Descomente para treinar modelo
+# log_reg = SklearnClassifier(LogisticRegression())
+# log_reg.train(training_set)
+# p1 = open("log_reg.pickle", "wb")
+# pickle.dump(log_reg,p1)
+# p1.close()
+# print("Classifier accuracy percent:",(nltk.classify.accuracy(log_reg, testing_set))*100)
+
+# Descomente para treinar modelo
+# onb = nltk.NaiveBayesClassifier.train(training_set)
+# p2 = open("onb.pickle", "wb")
+# pickle.dump(onb,p2)
+# p2.close()
+
+# Printando acurácia e features
+# print("Classifier accuracy percent:",(nltk.classify.accuracy(onb, testing_set))*100)
+# classifier.show_most_informative_features(10)
+
+# Descomente para treinar modelo
+# rf = SklearnClassifier(RandomForestClassifier())
+# rf.train(training_set)
+# p3 = open("rf.pickle", "wb")
+# pickle.dump(rf,p3)
+# p3.close()
+# print("Classifier accuracy percent:",(nltk.classify.accuracy(rf, testing_set))*100)
+
+# Descomente para treinar modelo
+# sgd = SklearnClassifier(SGDClassifier())
+# sgd.train(training_set)
+# p4 = open("sgd.pickle", "wb")
+# pickle.dump(sgd,p4)
+# p4.close()
+# print("Classifier accuracy percent:",(nltk.classify.accuracy(sgd, testing_set))*100)
+
+# Descomente para treinar modelo
+# svc = SklearnClassifier(SVC())
+# svc.train(training_set)
+# p5 = open("svc.pickle", "wb")
+# pickle.dump(svc,p5)
+# p5.close()
+# print("Classifier accuracy percent:",(nltk.classify.accuracy(svc, testing_set))*100)
